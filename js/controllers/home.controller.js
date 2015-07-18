0fc5e9b41400515b25f7678a35cfc4b6a8d44ec4
nguyenminhhoang <nguyenminhhoang210891@gmail.com>
2015-07-18 23:06:51 +0700

    create new contact from home page

--- js/controllers/home.controller.js
+++ js/controllers/home.controller.js
@@ -2,22 +2,34 @@
   angular.module('app')
     .controller('homeCtrl', ['$rootScope','$scope', 'contactDB', 
       function($rootScope, $scope, contactDB) {
-      var getDataDone = false;
+      //var getDataDone = false;
       
-      $scope.customers = [];
+      /*$scope.customers = [];
       $rootScope.$on('searchBar:filter', function(event, data) {
-        console.log(data);
         contactDB.find(data.name, data.key)
         .then(function(res) {
           $scope.customers = res;
           getDataDone = true;
         });
-      });
-      $scope.onItemClick = function(customer) {
+      });*/
+      /*$scope.onItemClick = function(customer) {
         $scope.go('contact-show', {id: customer._id});
       };
       $scope.isEmpty = function() {
         return getDataDone && $scope.customers.length === 0;
-      };
+      };*/
+
+      $scope.data = {};
+      $scope.onSubmit = function(form) {
+        if(form.$valid) {
+          contactDB.save($scope.data)
+          .then(function() {
+            alert('Lưu thành công!');
+            $scope.data = {};
+          }, function(err) {
+            alert(err);
+          });
+        }
+        };
     }]);
 })();
